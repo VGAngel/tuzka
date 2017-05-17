06bd73db48cea8c20f256452b00f275675163b13
Valentin Polischuk <vpolischuk@newagesol.com>
2017-05-17 10:10:39 +0300

    brr

---
+++ try_class.lua
@@ -0,0 +1,23 @@
+local class = require "middleclass"
+
+local Fruit = class('Fruit')
+
+function Fruit:initialize(sweetness)
+  self.sweetness = sweetness
+end
+
+Fruit.static.sweetness_threshold = 5
+
+function Fruit:isSweet()
+  return self.sweetness > Fruit.sweetness_threshold
+end
+
+local Lemon = class('Lemon', Fruit)
+
+function Lemon:initialize()
+  Fruit.initialize(self, 1)
+end
+
+local lemon = Lemon:new()
+
+print(lemon:isSweet())
